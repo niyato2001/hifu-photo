e307628b5c765da2106083507e4b37ddd6641e66
niyato2001 <niyato2001@gmail.com>
2023-02-20 18:36:44 +0900

    feat: input でひとまずファイルを upload

--- src/component/common/template/PageMain/PageMain.tsx
+++ src/component/common/template/PageMain/PageMain.tsx
@@ -20,14 +20,23 @@ export const PageMain: React.FC<PageMainPresenterProps> = ({
   <>
     <h1 className='text-center text-2xl font-bold text-black'>皮膚科専攻医の写真整理を DX する</h1>
     <div className='my-10 mx-auto flex w-80 flex-col gap-10'>
-      <button
+      {/* <button
         onClick={
           handleLoad
           // () => console.log('hello')
         }
-        className={`btn-file btn`}
+        className='btn-file btn'
       >
         画像ファイルを選択
+      </button> */}
+      {/* inputを包含したbuttonでファイルを選択できたらhandleLoadを作動 */}
+      <button className='btn-file btn relative inline-block max-w-fit'>
+        画像ファイルを選択
+        <input
+          type='file'
+          className='absolute left-0 top-0 h-full w-full opacity-0'
+          onChange={handleLoad}
+        />
       </button>
       {/* <Button {...readButton} />
       <Button {...renameButton} /> */}
